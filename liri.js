809b47a460af88a3418b8788350d2267fc4f7641
Robert (Wynn) Blevins <wynnblevins@yahoo.com>
2018-04-06 13:04:48 -0400

    Worked on getting do what it says command to run as expected (seems to be working).  Added .vscode file for debugging purposes.

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
+var fs = require('fs');
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
-var Request = require('request');
+var request = require('request');
 var keys = require('./keys');
 var spotifyClient = new Spotify(keys.spotify);
 var twitterClient = new Twitter(keys.twitter);
@@ -13,16 +14,20 @@ var command = {
     movie: 'MOVIE-THIS'
 };
 
-// converting command to upper case by default for sanity's sake
 function getUserCommand() {
     var userCommand = process.argv[2];
+    // converting command to upper case by default for sanity's sake
     userCommand = userCommand.toUpperCase();
     return userCommand;
 }
 
 function getUserCommandArg() {
-    var userCommandArg = process.argv[3];
-    return userCommandArg;
+    var userCommandArg = process.argv.slice(3);
+    return userCommandArg.join(' ');
+}
+
+function getCommandArgFromFile() {
+
 }
 
 function showTweets() {
@@ -41,41 +46,96 @@ function showTweets() {
     });
 }
 
-function doWhatItSays() {
-    console.log('Inside doWhatItSays function.');
+function doWhatItSays(fileName) {
+    var command = null, 
+        arg = null, 
+        openQuoteNdx = null,
+        closeQuoteNdx = null;
+    
+    if (!fileName) {
+        fileName = "random.txt";
+    }
+    
+    fs.readFile(fileName, "utf8", function(error, data) {
+        data = data.split('');
+        var commaNdx = data.indexOf(',');
+        var firstString = data.slice(0, commaNdx).join('');
+        var secondString = data.slice(commaNdx).join('');
+        handleInput(firstString, secondString);
+    });    
 }
 
 function spotify(songName) {
     spotifyClient.search({ 
         type: 'track', 
         query: songName,
-        limit: 1
+        limit: 10
     }).then(function(response) {
         console.log(response);
+        // show the artist name
+
+        // show the preview link from Spotify
+
+        // show the album the song is off of
     }).catch(function(err) {
         console.log(err);
     });
 }
 
-function movie() {
-    console.log('Inside movie function.');
+function movie(title) {
+    // form the omdb request url per documentation found here: omdbapi.com
+    var apiKey = 'apikey=trilogy';
+    var baseUrl = 'http://www.omdbapi.com/?';
+    var plot = 'plot=full';
+    var title = 't=' + title;
+    var returnType = 'type=json';
+    var fullUrl = baseUrl + apiKey + '&' + title + '&' + plot;
+    
+    var ratings = {
+        imdb: 0,
+        rottenTomatoes: 1,
+        metacritic: 2
+    };
+
+    request(fullUrl, function (error, response, body) {
+        var omdbResponse = JSON.parse(body);
+        console.log('Title: ' + omdbResponse.Title);
+        console.log('Release Year: ' + omdbResponse.Released);
+        console.log('IMDB Rating: ' + omdbResponse.Ratings[ratings.imdb].Value);
+        console.log('Rotten Tomatoes Rating: ' + omdbResponse.Ratings[ratings.rottenTomatoes].Value);
+        console.log('Country: ' + omdbResponse.Country);
+        console.log('Language: ' + omdbResponse.Language);
+        console.log('Plot: ' + omdbResponse.Plot);
+        console.log('Actors: ' + omdbResponse.Actors);
+    });
 }
 
-switch (getUserCommand()) {
-    case command.tweets: 
-        showTweets();
-        break;
-    case command.do:
-        doWhatItSays();
-        break;
-    case command.spotify: 
-        var songName = getUserCommandArg();
-        spotify(songName);
-        break;
-    case command.movie:
-        movie();
-        break;
-    default: 
-        console.log('Unknown command entered.');
-        break; 
+function handleInput(userCommand, commandArg) {
+    userCommand = userCommand.toUpperCase();
+    
+    switch (userCommand) {
+        case command.tweets: 
+            showTweets();
+            break;
+        case command.do:
+            doWhatItSays();
+            
+            break;
+        case command.spotify: 
+            if (!commandArg) {
+                commandArg = getUserCommandArg();
+            }
+            
+            spotify(commandArg);
+            break;
+        case command.movie:
+            var movieTitle = getUserCommandArg();
+            movie(movieTitle);
+            break;
+        default: 
+            console.log('Unknown command entered.');
+            break; 
+    }
 }
+
+handleInput(getUserCommand());
